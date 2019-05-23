dcc94749c7aa771a95ccc48bbb62dd1a1d9eb266
pachel <pachel82>
2019-05-23 21:26:32 +0200

    fatfree compatible

--- examples/fatfree/index.php
+++ examples/fatfree/index.php
@@ -17,75 +17,7 @@ $f3->set("db",dbClass::instance());
 
 $f3->route('GET /',
 	function($f3) {
-		$classes=array(
-			'Base'=>
-				array(
-					'hash',
-					'json',
-					'session',
-					'mbstring'
-				),
-			'Cache'=>
-				array(
-					'apc',
-					'apcu',
-					'memcache',
-					'memcached',
-					'redis',
-					'wincache',
-					'xcache'
-				),
-			'DB\SQL'=>
-				array(
-					'pdo',
-					'pdo_dblib',
-					'pdo_mssql',
-					'pdo_mysql',
-					'pdo_odbc',
-					'pdo_pgsql',
-					'pdo_sqlite',
-					'pdo_sqlsrv'
-				),
-			'DB\Jig'=>
-				array('json'),
-			'DB\Mongo'=>
-				array(
-					'json',
-					'mongo'
-				),
-			'Auth'=>
-				array('ldap','pdo'),
-			'Bcrypt'=>
-				array(
-					'mcrypt',
-					'openssl'
-				),
-			'Image'=>
-				array('gd'),
-			'Lexicon'=>
-				array('iconv'),
-			'SMTP'=>
-				array('openssl'),
-			'Web'=>
-				array('curl','openssl','simplexml'),
-			'Web\Geo'=>
-				array('geoip','json'),
-			'Web\OpenID'=>
-				array('json','simplexml'),
-			'Web\Pingback'=>
-				array('dom','xmlrpc')
-		);
-		$f3->set('classes',$classes);
-		$f3->set('content','welcome.htm');
-		echo View::instance()->render('layout.htm');
+		echo View::instance()->render('layout.html');
 	}
 );
-
-$f3->route('GET /userref',
-	function($f3) {
-		$f3->set('content','userref.htm');
-		echo View::instance()->render('layout.htm');
-	}
-);
-
 $f3->run();
